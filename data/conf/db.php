8345aa1427ad607106f9a2781fcf9e1225ee0b70
ETY001 <ety001@domyself.me>
2017-05-03 00:04:48 +0800

    data

---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'qiye',
+    'DB_USER' => 'root',
+    'DB_PWD' => '123456',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'qiye_',
+    //密钥
+    "AUTHCODE" => 'M1gWnHAxU4pcmH8esR',
+    //cookies
+    "COOKIE_PREFIX" => 'Qb7pHg_',
+);
